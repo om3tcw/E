cec5e12254840853faaf8388d11852e65fc7738e
jaffyjaffy <jaffyjaffy11@gmail.com>
2024-12-20 14:19:22 -0500

    Revert "refactored a couple files, new js folder. "

--- js/hover_on_play_om3tcw.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-
-const mikoDing = document.createElement("audio");
-mikoDing.setAttribute('src','https://cdn.jsdelivr.net/gh/om3tcw/r@emotes/soundposts/sounds/om3tcw.ogg');
-mikoDing.loop = true;
-mikoDing.volume = 0.1;
-document.getElementsByClassName("navbar-brand")[0].onmouseenter = () => mikoDing.play();
-document.getElementsByClassName("navbar-brand")[0].onmouseleave = () => mikoDing.pause();
